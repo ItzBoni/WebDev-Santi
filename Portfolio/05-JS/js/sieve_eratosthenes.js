8511638c3604183251bfcaa24423e22daa8e6302
Santiago Bonilla <112128287+ItzBoni@users.noreply.github.com>
2025-10-10 16:29:08 -0600

    eratosthenes is done :)

--- Portfolio/05-JS/js/sieve_eratosthenes.js
+++ Portfolio/05-JS/js/sieve_eratosthenes.js
@@ -5,17 +5,33 @@
 
 // TODO: Adjust this script so it can work with the sieve.html file.
 
-var sieve = function (n) {
+var sieve = function () {
   "use strict";
+    function addToSieve(n) {
+    var array = []
+    var boolArray = new Array(n).fill(false);
+    let i;
+    for (i = 2; i <= Math.sqrt(n); i++) {
+      let j;
+      for (j = i+i; j < n; j+=i){
+        boolArray[j] = true;
+      }
+    }
+    
+    for(i = 2; i < boolArray.length; i++){
+      if (!boolArray[i]){
+        array.push(i);
+      }
+    }
 
-  var array = [],
-    primes = [],
-    i,
-    j;
+    return array
+  }
 
+  let num = Number(document.getElementById("num").value);
+  let primes = addToSieve(num);
+  document.getElementById("primes").innerHTML = primes;
+
+  console.log(primes)
   // TODO: Implement the sieve of eratosthenes algorithm to find all the prime numbers under the given number.
-
-  return primes;
 };
 
-console.log(sieve(1000000));
